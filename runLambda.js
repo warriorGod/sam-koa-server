4e524760ef17b9dc78da7a4d2a5c0e28b72dd562
Gregor Tramsak <gregor.tramsak@revlifter.com>
2019-05-15 12:25:56 +0100

    Use debug for lambda-local messages instead of the console.log

--- runLambda.js
+++ runLambda.js
@@ -1,4 +1,5 @@
-const debug = require('debug')('app:runLambda');
+const debug = require('debug')('sks:runLambda');
+const debugLambdaLocal = require('debug')('sks:lambda-local');
 
 // function to generate functional api test requests
 function lambdaEvent(method, endpoint, queryParams, body, other) {
@@ -14,8 +15,40 @@ function lambdaEvent(method, endpoint, queryParams, body, other) {
     }, other);
 }
 
+function createCustomLogger() {
+    // https://github.com/winstonjs/winston#transports
+    const winston = require('winston');
+    const Transport = require('winston-transport');
+
+    class DebugOutput extends Transport {
+        constructor(opts) {
+            super(opts);
+        }
+
+        log(info, callback) {
+            setImmediate(() => {
+                this.emit('logged', info);
+            });
+
+            // log lambda local under a debug namespace, not the console
+            debugLambdaLocal(info);
+            callback();
+        }
+    }
+
+    return winston.createLogger({
+        level: 'info',
+        format: winston.format.json(),
+        defaultMeta: { service: 'user-service' },
+        transports: [
+            new DebugOutput()
+        ]
+    });
+}
+
 function runLambda(absHandlerPath, eventPayload, handlerName) {
     let lambdalocal = require('lambda-local');
+    lambdalocal.setLogger(createCustomLogger());
     debug({absHandlerPath});
     let lambdaFunc = require(absHandlerPath);
     return lambdalocal.execute({
